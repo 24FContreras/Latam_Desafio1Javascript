daf5dc281941369a24482494c53ca9c4f75059a8
Franco Contreras <24.f.contreras@gmail.com>
2022-09-08 17:15:24 -0400

    First Commit

---
+++ assets/js/cardScript.js
@@ -0,0 +1,19 @@
+const precio = 400000;
+let cantidadUsuario = document.querySelector("#cantidadUsuario");
+let colorUsuario = document.querySelector("#colorUsuario");
+const submit = document.querySelector("#submit");
+let precioTotal = document.querySelector("#precioTotal");
+let cantidadTotal = document.querySelector("#cantidadTotal");
+let colorRing = document.querySelector("#colorRing");
+
+submit.addEventListener("click", function () {
+  precioTotal.innerHTML = Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
+  }).format(cantidadUsuario.value * precio);
+
+  cantidadTotal.innerHTML = cantidadUsuario.value;
+  colorRing.style.backgroundColor = colorUsuario.value;
+});
+
+precioTotal.innerHTML = cantidadUsuario.value * precio;
